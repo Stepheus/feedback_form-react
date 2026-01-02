8d557b598bed72585127acdbe70df273e4c26979
Stepheus <stephen.iboudah@outlook.com>
2026-01-01 20:03:25 -0500

    updates

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
+    const [formData, setFormData] = useState({
+        name:"", email: "", feedback: ""
+    });
 
+    const handleChange = (e) => {
+        const {name, value } = event.target;
+        setFormData({...formData, [name]: value});
+    };
   return (
     <>
     <nav>
@@ -11,7 +18,15 @@ const FeedbackForm = () => {
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input type= "text"
+                name="name"
+                placeholder="Your Name" onChange={}/>
+        <input type ="email"
+                name="email"
+                placeholder="Your Email"/>
+        <textarea name="feedback"
+                    placeholder='Your Feedback'/>
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
   );
